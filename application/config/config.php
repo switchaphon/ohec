8dbb0db4c1f70ba256ef55f7394eb338d9416944
Witchaphon Saeng-aram <s.witchaphon@gmail.com>
2017-12-25 23:24:34 +0700

    Updated at 2017-12-25

--- application/config/config.php
+++ application/config/config.php
@@ -39,8 +39,8 @@ $config['base_url'] = "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
-// $config['index_page'] = '';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
