32068b7b0641037b62dea1f1dc71dbda3f226f95
Witchaphon Saeng-aram <s.witchaphon@gmail.com>
2017-12-25 23:58:45 +0700

    add compress_and_resize_photo

--- application/libraries/Utilities.php
+++ application/libraries/Utilities.php
@@ -5,6 +5,28 @@ class Utilities {
 
     }
     
+    // function upload($uploadedFile = null , $config = null) {
+
+    //     //Assign the CodeIgniter object to a variable for using instead "$this"
+    //     $CI =& get_instance();
+        
+    //     $CI->load->library('upload', $config);
+    //     $CI->upload->initialize($config);
+
+    //     $massage = array();
+
+    //     if ( ! $CI->upload->do_upload($uploadedFile))
+    //     {   
+    //         // log_error("Upload file to ".$config['upload_path']." failed");
+    //         $massage[] = array('cmd'=>'Import', 'state'=>'danger', 'result'=> $CI->upload->display_errors() );
+    //     } else {
+    //         // log_info("Upload file to ".$config['upload_path']." success");
+    //         $massage[] = array('cmd'=>'Import', 'state'=>'success', 'result'=> $CI->upload->data() );
+    //     }
+    //     return $massage;
+     
+    // }
+
     function upload($uploadedFile = null , $config = null) {
 
         //Assign the CodeIgniter object to a variable for using instead "$this"
@@ -21,11 +43,31 @@ class Utilities {
             $massage[] = array('cmd'=>'Import', 'state'=>'danger', 'result'=> $CI->upload->display_errors() );
         } else {
             // log_info("Upload file to ".$config['upload_path']." success");
-            $massage[] = array('cmd'=>'Import', 'state'=>'success', 'result'=> $CI->upload->data() );
+            $data = $CI->upload->data();  
+            // echo $data['full_path']."<BR>";
+            $config['image_library'] = 'gd2';  
+            $config['source_image'] = $data['full_path'];  
+            // $config['create_thumb'] = FALSE;  
+            $config['maintain_ratio'] = TRUE;  
+            $config['quality'] = '100%';  
+            $config['width'] = 2048;  
+            $config['height'] = 1024;  
+            $config['x_axis'] = 2048;  
+            $config['y_axis'] = 1024;  
+
+            $CI->load->library('image_lib', $config);  
+
+            if ( ! $CI->image_lib->resize())
+            {
+                echo $CI->image_lib->display_errors();
+            }
+            // echo "xxx";
+            // $massage[] = array('cmd'=>'Import', 'state'=>'success', 'result'=> $CI->upload->data() );
+            $massage[] = array('cmd'=>'Import', 'state'=>'success', 'result'=> $CI->image_lib->resize() );
         }
         return $massage;
      
-    }
+    }    
 }
 
 ?>
